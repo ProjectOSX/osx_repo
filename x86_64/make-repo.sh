cf638e747a2627b03e0a89a55d1b91b1d5d84c65
CodeCraft <86466022+CodeCraftPlugin@users.noreply.github.com>
2022-09-18 11:38:44 +0000

    idk what but hope it works

--- x86_64/make-repo.sh
+++ x86_64/make-repo.sh
@@ -3,12 +3,10 @@
 rm osx_repo*
 
 echo "repo-add"
-repo-add -n -R OSX_Repo.db.tar.gz *.pkg.tar.zst
+repo-add OSX_Repo.db.tar.gz *.pkg.tar.zst
 
 sleep 1
 
-rm OSX_Repo.db.tar.gz.old
-rm OSX_Repo.files.tar.gz.old
 echo "####################################"
 echo "Repo Updated!!"
 echo "####################################"
